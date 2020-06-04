7eff65f94cfdfebdcb7d5195bc9bedc73822be04
Jeff Wong <v-jwong1@expedia.com>
2020-06-04 21:34:33 +1000

    simple express server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/hello", (req, res) => {
+  res.json({ 1: "Hello world!!!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}!`);
+});
