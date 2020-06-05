a22fccb8d300fc6f9ae31b7fec947166a591fd78
Jeff Wong <v-jwong1@expedia.com>
2020-06-05 16:31:46 +1000

    add another route in index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get("/a", (req, res) => {
   res.json({ 1: "Hello world!!!" });
 });
 
+app.get("/b", (req, res) => {
+  res.json({ 2: "JS is the best!" });
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}!`);
 });
