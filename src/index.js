2ca20947416a9e26397c6c55f61d9ed7658d693c
Jeff Wong <v-jwong1@expedia.com>
2020-06-12 21:41:28 +1000

    set up main index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors({ origin: "http://jeffs.xyz:11003" }));
+
 app.get("/a", (req, res) => {
-  res.json({ 1: "Hello world!!!" });
+  console.log("Request coming in for endpoint a!")
+  res.json([{ id: 9002, name: "Martin Lombok" }, { id: 9005, name: "Kevin Nam" }]);
 });
 
 app.get("/b", (req, res) => {
+  console.log("Request coming in for endpoint b!")
   res.json({ 2: "JS is the best!" });
 });
 
