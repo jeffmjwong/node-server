adff5d590f1c902d260d44b5dbba1ff8517fea83
Jeff Wong <v-jwong1@expedia.com>
2020-07-09 22:13:29 +1000

    add another test

--- test/test1.spec.js
+++ test/test1.spec.js
@@ -2,4 +2,8 @@ describe("test 1", () => {
   it("adds 2 numbers correctly", () => {
     expect(1 + 1).toEqual(2);
   });
+
+  it("subtracts 2 numbers correctly", () => {
+    expect(2 - 1).toEqual(1);
+  });
 });
