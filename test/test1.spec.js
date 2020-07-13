f22325503a83dad98e6cd95f25d49dd3e1b67254
Jeff Wong <v-jwong1@expedia.com>
2020-07-13 21:11:30 +1000

    new PR to test jenkins PR job

--- test/test1.spec.js
+++ test/test1.spec.js
@@ -1,9 +1,9 @@
 describe("test 1", () => {
   it("adds 2 numbers correctly", () => {
-    expect(1 + 1).toEqual(2);
+    expect(1 + 1).toEqual(3);
   });
 
   it("subtracts 2 numbers correctly", () => {
-    expect(2 - 1).toEqual(1);
+    expect(2 - 1).toEqual(2);
   });
 });
