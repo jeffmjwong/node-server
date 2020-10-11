7669b2ccb614d18a87695d0794ac0b38da83eb6e
Jeff Wong <v-jwong1@expedia.com>
2020-10-11 21:11:32 +1000

    change main code

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ app.use(cors());
 
 app.get("/a", (req, res) => {
   console.log("Request coming in for endpoint a!")
-  res.json([{ id: 1, name: "Martin Lombok" }, { id: 2, name: "Kevin Nam" }]);
+  res.json([{ id: 1, name: "Toyota Camry", price: "$20,000" }, { id: 2, name: "Mercedes CLS", price: "$50,000" }]);
 });
 
 
 app.get("/b", (req, res) => {
   console.log("Request coming in for endpoint b!")
-  res.json({ 2: "JS is the best!" });
+  res.json({ language: "JavaScript", description: "JS is a programming language created in 10 days." });
 });
 
 app.listen(port, () => {
